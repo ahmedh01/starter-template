bcc3310f0412339460709a8c8f23c07a8d89cb50
Ahmed Hashim <ahmed@cygnismedia.com>
2021-07-10 15:20:20 -0700

    bump up everything to latest stable versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
     rules: {
         // override/add rules settings here, such as:
         'vue/no-unused-vars': 'error',
+        'vue/script-setup-uses-vars': 'off',
     },
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,3 @@
 <template>
     <h1 class="mb-6 text-3xl font-extrabold">Home</h1>
 </template>
-
-<script setup></script>
